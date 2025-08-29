5e530b0df7c556437ed454de3b10be7f1b927f80
sahilkr14-cmd <sahil5678sharma@gmail.com>
2025-08-29 21:07:58 +0530

    Add Submit function, New User Image, and Sent Message

--- Port.js
+++ Port.js
@@ -6,15 +6,31 @@ function opentab(tabname) {
     for (tab of tabs) {
         tab.classList.remove("active-link");
     }
-    for (tabcont of tabconts){
+    for (tabcont of tabconts) {
         tabcont.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
-function openmenu(){
+function openmenu() {
     sidemeu.style.right = "0";
 }
-function closemenu(){
+function closemenu() {
     sidemeu.style.right = "-200px";
 }
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw-yN1aWkEkmjbICorKtIx3FT9Yncg47Hjd2nWVuZDD2MagSVGQgM0F20gnEvLZWkWCzw/exec'
+const form = document.forms['submit-to-google-sheet']
+const mess = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            mess.innerHTML = '<i class="fa-solid fa-paper-plane"></i> Message Sent Successfully. '
+            setTimeout(function(){
+                mess.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
